807720ccddad7e6e6d9df759a43a86dfb7181db9
dorvk <dorukcoban@outlook.com>
2021-01-30 22:55:58 +0300

    Initial Commit

---
+++ SwiftUITabBar/SwiftUITabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITabBarApp.swift
+//  SwiftUITabBar
+//
+//  Created by Doruk Çoban on 30.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
